bcaa27f42be56aecfab9c17a9e1d916e3dc5bddf
p-saurer <philipp.saurer1988@gmail.com>
2022-01-31 11:06:55 +0100

    Change HomePage widget to use localized title

--- lib/main.dart
+++ lib/main.dart
@@ -39,24 +39,17 @@ class MyApp extends StatelessWidget {
         Locale('de', ''), // German, no country code
       ],
 
-      home: const MyHomePage(title: "Lunch me!"),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -86,9 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(AppLocalizations.of(context)!.greeting),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
