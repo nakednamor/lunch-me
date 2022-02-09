299dc638060e49ae9b71c72a9dfc4a4a0636d36d
p-saurer <philipp.saurer1988@gmail.com>
2022-02-09 08:54:42 +0100

    WIP: First rendering of list items

--- lib/main.dart
+++ lib/main.dart
@@ -64,18 +64,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  late List<TagGroupWithTags> tagGroupsWithTags;
+  int count = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -83,16 +73,22 @@ class _MyHomePageState extends State<MyHomePage> {
 
     final Locale appLocale = Localizations.localeOf(context);
 
-    var tagGroupsWithTags = database.getAllTagsWithGroups(appLocale);
+    var tagGroupsWithTagsFuture = database.getAllTagsWithGroups(appLocale);
 
-    tagGroupsWithTags.then((value) {
-      for (var tagGroupWithTags in value) {
-        debugPrint("group: ${tagGroupWithTags.tagGroup}");
-        for (var tag in tagGroupWithTags.tags) {
-          debugPrint("tag: $tag");
-        }
-      }
+    tagGroupsWithTagsFuture.then((tagGroupsWithTags) {
+      setState(() {
+        this.tagGroupsWithTags = tagGroupsWithTags;
+        this.count = tagGroupsWithTags.length;
+      });
     });
+    // tagGroupsWithTags.then((value) {
+    //   for (var tagGroupWithTags in value) {
+    //     debugPrint("group: ${tagGroupWithTags.tagGroup}");
+    //     for (var tag in tagGroupWithTags.tags) {
+    //       debugPrint("tag: $tag");
+    //     }
+    //   }
+    // });
 
     var recipesList = database.getAllRecipeWithTags();
     recipesList.then((recipes) => {
@@ -110,41 +106,49 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.greeting),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              AppLocalizations.of(context)!.buttonCount,
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: getTagListView(),
+    );
+  }
+
+  // Item of the ListView
+  Widget tagItems(groupIndex) {
+    var tagGroup = tagGroupsWithTags[groupIndex];
+    int tagCount = tagGroup.tags.length;
+
+    if(tagCount > 0) {
+      return ListView.builder(
+          itemCount: tagCount,
+          itemBuilder: (BuildContext context, int tagIndex) {
+            return Text(tagGroup.tags[tagIndex].label);
+          },
+        shrinkWrap: true, // TODO provide fixed size/layout
+      );
+    }
+
+    return Text("no tags found in this group"); // TODO translate
+  }
+
+  ListView getTagListView() {
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int index) {
+          return Column(
+            children: [
+              // The header
+              Container(
+                padding: const EdgeInsets.all(10),
+                color: Colors.amber,
+                child: Text(tagGroupsWithTags[index].tagGroup.label),
+              ),
+
+              // The first list item
+              tagItems(index)
+            ],
+          );
+        //return tagItems(index);
+      },
+      shrinkWrap: true, // TODO provide fixed size/layout
     );
   }
 }
+
