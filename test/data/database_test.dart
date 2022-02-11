215fc5da41baf895836312d831e11541d42820cf
Roman Zillinger <r.zillinger@zoovu.com>
2022-02-11 22:02:52 +0100

    tag-groups and tags can be ordered now

--- test/data/database_test.dart
+++ test/data/database_test.dart
@@ -18,18 +18,38 @@ void main() {
   });
 
   test('should return all languages', () async {
+    // when
     var languages = await testDatabase.getAllLanguages();
+
+    // then
     expect(languages.length, 2);
     expect(languages.map((e) => e.lang), containsAll(['de', 'en']));
   });
 
   test('should return all recipes', () async {
+    // when
     var recipeWithTags = await testDatabase.getAllRecipeWithTags();
-    expect(recipeWithTags.length, 8);
 
+    // then
+    expect(recipeWithTags.length, 8);
     expect(recipeWithTags[4].recipe.name, "Erdäpfel-Paprika Gulasch");
 
     var tags = recipeWithTags[4].tags.map((e) => e.id);
     expect(tags, containsAll([2, 3, 5, 7]));
+  });
+
+  test('should return tag-groups and tags ordered by order-column', () async {
+    //when
+    var actual = await testDatabase.getAllTagsWithGroups(const Locale("en"));
+
+    // then tag-groups should be ordered
+    var tagGroupIds = actual.map((e) => e.tagGroup.tagGroup);
+    expect(tagGroupIds, containsAllInOrder([2, 1, 3]));
+
+    // and tags within group 'Type' should be ordered
+    var tagGroupTime =
+        actual.firstWhere((tagGroup) => tagGroup.tagGroup.tagGroup == 2);
+    var tagsOfTagGroupTime = tagGroupTime.tags.map((e) => e.tag);
+    expect(tagsOfTagGroupTime, containsAllInOrder([5, 12, 13, 6, 7, 8, 9, 10]));
   });
 }
