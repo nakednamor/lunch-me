dba23d22a906f8a2d6217e2dc2bf9c1437d93d98
Roman Zillinger <roman.zillinger@gmail.com>
2022-08-29 20:14:02 +0200

    add missing not-null constraint to label column of tables tags and tag_groups

--- doc/db_init/create_database.sql
+++ doc/db_init/create_database.sql
@@ -1,8 +1,8 @@
 PRAGMA foreign_keys = ON;
 
-create table tag_groups ( id integer primary key autoincrement, ordering int not null, label varchar(50), unique(ordering), unique(label) );
+create table tag_groups ( id integer primary key autoincrement, ordering int not null, label varchar(50) not null, unique(ordering), unique(label) );
 
-create table tags ( id integer primary key autoincrement, tag_group int not null, ordering int not null, label varchar(50), unique(tag_group, ordering), unique(tag_group, label), foreign key(tag_group) references tag_groups(id) on delete cascade);
+create table tags ( id integer primary key autoincrement, tag_group int not null, ordering int not null, label varchar(50) not null, unique(tag_group, ordering), unique(tag_group, label), foreign key(tag_group) references tag_groups(id) on delete cascade);
 
 create table recipes ( id integer primary key autoincrement, name varchar(50) not null, type int not null, url varchar(255), image varchar(255), image_photo varchar(50), content_photo varchar(50), unique(name, type) );
 
